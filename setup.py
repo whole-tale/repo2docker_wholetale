2406f07e7a4d60f046cf98fbb6d6d7eff4572f45
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2020-09-16 10:40:21 -0500

    Bump master to dev version

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/whole-tale/repo2docker_wholetale',
-    version='0.9.1',
+    version='0.10.0.dev0',
     zip_safe=False,
 )
