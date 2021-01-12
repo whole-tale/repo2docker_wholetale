1502c7a32a699f5a451066735df55904e98c4bbe
Craig Willis <willis8@illinois.edu>
2021-01-12 22:38:19 +0000

    Remove CSP handling

--- repo2docker_wholetale/iframes/jupyter_notebook_config.py
+++
@@ -1,9 +0,0 @@
-import os
-csp = os.environ.get(
-    'CSP_HOSTS', ''
-)
-c.NotebookApp.tornado_settings = {
-    'headers': {
-        'Content-Security-Policy': "frame-ancestors {} 'self' ".format(csp.replace('"', ''))
-    }
-}
