432e1837f4f97977867c9587ab133b96b579d931
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2021-04-02 12:26:12 -0500

    Set version to v1.0rc1

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/whole-tale/repo2docker_wholetale',
-    version='0.10.0dev0',
+    version='1.0rc1',
     zip_safe=False,
 )
