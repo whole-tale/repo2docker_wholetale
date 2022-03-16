16a5c012d4733e77110f7d463cca09ded797b853
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2022-03-16 10:38:45 -0500

    Bump version to 1.1.post0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/whole-tale/repo2docker_wholetale',
-    version='1.1',
+    version='1.1.post0',
     zip_safe=False,
 )
